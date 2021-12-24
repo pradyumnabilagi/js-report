aff39d2f909c2b58d1e267eef912cf6f4ebb7154
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2021-12-24 09:48:56 +0530

    pdfmake

--- src/classes/create-pdf.ts
+++ src/classes/create-pdf.ts
@@ -1,28 +1,10 @@
-import puppeteer, { LaunchOptions, BrowserConnectOptions, BrowserLaunchArgumentOptions } from 'puppeteer'
+
 import hbs from 'handlebars'
-// import chromium from "chrome-aws-lambda"
-type PuppeteerLounchOptions =  BrowserLaunchArgumentOptions & LaunchOptions & BrowserConnectOptions ;
+import htmltoPdfMake from "html-to-pdfmake"
+
 
 export default class CreatePdf{
-    private pdfOptions !: puppeteer.PDFOptions
-    private lounch!:PuppeteerLounchOptions
 
-
-    /**
-     * 
-     * @param _pdfOtpions Puppeteers pdfoprions
-     * @param _lounch  Puppeteer Lounch Options
-     */
-    constructor(_pdfOtpions: puppeteer.PDFOptions, _lounch?:PuppeteerLounchOptions){
-        this.pdfOptions= _pdfOtpions
-        if(_lounch !== undefined){
-            this.lounch = _lounch
-        }
-       
-    }
-
-
-    
     /**
      * This comples the html
      * @param html string
@@ -41,42 +23,34 @@ export default class CreatePdf{
      * @returns buffer
      */
     create = async (html:string, data?:any):Promise<Buffer> => {
-        let browser = await puppeteer.launch(this.lounch);
-        let page = await browser.newPage();
-        try {
-            let source =  this.compileHtmlString(html, data)
-            await page.setContent(source);
-            let pdf =  await page.pdf(this.pdfOptions)
-            return pdf
-        } catch (error) {
-            throw error
-        }finally{
-            console.log("closed pdf")
-            await page.close()
-            await browser.close()
-        }
+        // convert html to pdfmake string
+        let source =  this.compileHtmlString(html, data)
+        const pdfmakeData = htmltoPdfMake(source)
         
        
 
     }
 
-        /**
-     * creates the of PDF file from chrome-aws-lambda
-     * More suted for serveress appliation like next js
-     * @param html string
-     * @param data data to handlebars 
-     * @returns buffer
-     */
-    // createFromChromium = async (html:string, data?:any):Promise<Buffer> => {
-    //     let browser = await chromium.puppeteer.launch(this.lounch);
+    // create = async (html:string, data?:any):Promise<Buffer> => {
+    //     let browser = await puppeteer.launch(this.lounch);
     //     let page = await browser.newPage();
-    //     let source =  this.compileHtmlString(html, data)
-    //     await page.setContent(source);
-    //     let pdf =  await page.pdf(this.pdfOptions)
-    //     await page.close()
-    //     await browser.close()
-    //     return pdf
+    //     try {
+    //         let source =  this.compileHtmlString(html, data)
+    //         await page.setContent(source);
+    //         let pdf =  await page.pdf(this.pdfOptions)
+    //         return pdf
+    //     } catch (error) {
+    //         throw error
+    //     }finally{
+    //         console.log("closed pdf")
+    //         await page.close()
+    //         await browser.close()
+    //     }
+        
+       
+
     // }
+
 
 }
 
