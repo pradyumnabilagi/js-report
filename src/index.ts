d5b14bcc9a55080dd444316a307fe0f9ed2e71db
umeshramya <umeshbilagi@gmail.com>
2025-02-02 00:56:07 +0530

    working with papersize

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import CreatePdf, { PDF_HEADER } from "./classes/create-pdf";
 // import { PDFOptions, PaperFormat } from "puppeteer"
 
+// Import paper-size module
+// @ts-ignore
+import paperSize from 'paper-size';
+export {paperSize}
+
 export { CreatePdf, PDF_HEADER };
 // export type { PaperFormat }
